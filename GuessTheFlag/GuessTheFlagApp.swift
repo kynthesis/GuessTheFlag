e0d08818caa9321973cc10c097b125fa77ffdf53
Khoa D. Vo <khoa.synthesis@gmail.com>
2024-01-09 22:28:04 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Khoa D. Vo on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
